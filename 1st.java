b1d02703ab07ce9ecff51d0caf03f055381d7800
RobzLegz <robzlegz@gmail.com>
2022-10-11 18:18:59 +0300

    first commit

---
+++ 1st.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.print("Enter text: ");
+    String text = in.nextLine();
+
+    int word_amount = text.split(" ").length;
+
+    System.out.print("Enter symbol: ");
+    String symbol = in.nextLine();
+
+    if (symbol.length() != 1) {
+      System.out.print("Invalid symbol, try again: ");
+      symbol = in.nextLine().toLowerCase();
+    }
+
+    String newText = text.replaceAll(symbol, "");
+
+    System.out.println("There are " + word_amount + " words in this text");
+    System.out.println("New text:");
+    System.out.println(newText);
+
+    in.close();
+  }
+}
